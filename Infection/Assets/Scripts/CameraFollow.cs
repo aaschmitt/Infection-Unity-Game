d66cfba71fb0332dc3b28f94f72b40fd11609eb2
Audrey Schmitt <audrey.schmitt07@gmail.com>
2020-11-14 16:50:39 -0500

    Added CameraFollow and temporary background

---
+++ Infection/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    /* Serialized Private Fields */
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime = 0.3F;
+    [SerializeField] private Vector3 offset;
+    
+    /* Private Fields */
+    private Vector3 _velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = target.TransformPoint(offset);                                                               // Define a target position offset to the target being fallowed
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);        // Smoothly move the camera towards target position
+    }
+}
