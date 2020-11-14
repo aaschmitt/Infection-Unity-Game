3705ac590ae0f454ef8d80b025b67aa2f1354b3b
Audrey Schmitt <audrey.schmitt07@gmail.com>
2020-11-14 16:13:33 -0500

    Added basic player movement

--- Infection/Assets/Scripts/PlayerController.cs
+++ Infection/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    /* Serialized Field Private Fields */
+    [SerializeField] private float speed = 5.0f;
+    
     void Update()
     {
+        HandleInput();
+    }
+    
+    /* Reads input and moves Player accordingly */
+    private void HandleInput()
+    {
+        /* Move Up */
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(speed * Time.deltaTime * Vector3.up);
+        }
         
+        /* Move Down */
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(speed * Time.deltaTime * Vector3.down);
+        }
+        
+        /* Move Left */
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(speed * Time.deltaTime * Vector3.left);
+        }
+        
+        /* Move Right */
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime * Vector3.right);
+        }
     }
 }
